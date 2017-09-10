025fdf6895382413828c1e80cdc55576ef787180
Majid Mokhtari <majidmokht@gmail.com>
2017-09-10 15:16:34 -0700

    refactor

--- Tamasha-ui/src/components/Slider.js
+++ Tamasha-ui/src/components/Slider.js
@@ -20,7 +20,7 @@ class Slider extends Component {
         }
     }
 
-    _renderSlider(){
+    _renderSliderItems(){
         const {imagesSlider} = this.state;
         return imagesSlider.map((item, i) => (
             <View key={i}>
@@ -33,40 +33,14 @@ class Slider extends Component {
     render(){
         return (
             <Swiper style={styles.wrapper} showsButtons={true}>
-                {this._renderSlider()}
+                {this._renderSliderItems()}
             </Swiper>
         )
     }
 }
 
 const {width} = Dimensions.get('window');
-
 var styles = StyleSheet.create({
-    wrapper: {
-    },
-    slide1: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#9DD6EB',
-    },
-    slide2: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#97CAE5',
-    },
-    slide3: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#92BBD9',
-    },
-    text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold',
-    },
     image: {
         width: width,
         height: '100%', 
